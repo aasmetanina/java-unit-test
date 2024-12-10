3ccc6df104b96462b0279b55744164ab4dc91e4d
Anna Smetanina <aasmetanina@yandex.ru>
2024-12-10 22:28:41 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,7 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+    
+	assertEquals("Пользователю меньше, чем 18 лет", true, isAdult);
+}
